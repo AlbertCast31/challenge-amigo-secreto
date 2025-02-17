3c9548b68c9e508b9c24891c6191f2e15e235ef9
AlbertCast31 <beto31cast@gmail.com>
2025-02-16 20:22:14 -0600

    Creamos una función para almacenar los nombres y validar que no este vacia por medio de un alert

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//1 Generamos una lista para almacenar los nombres ingresados
+let listamigos=[];
+
+//2 Creamos una función para agregar los nombres  a la lista por medio del DOM
+function agregarAmigo(){
+    let input=document.getElementById("amigo");
+    let nombre= input.value.trim(); // elimina los espacios en blanco al inicio y al final 
+
+    if (nombre=== ""){
+        alert("Por favor, ingresa un nombre valido.");
+        return;
+    }
+}
+
